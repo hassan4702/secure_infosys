1004d5c8c4369b41d423a352b83a4ec38aa1848d
hassan4702 <muhammadhassanshakil35066@gmail.com>
2024-05-11 19:37:48 +0500

    🚨 refactor: Remove unused toast component

--- components/toast.jsx
+++
@@ -1,9 +0,0 @@
-import { useToast } from "@/components/ui/use-toast";
-
-export function toast(message, description) {
-  const { toast } = useToast();
-  toast({
-    title: message,
-    description: description,
-  });
-}
